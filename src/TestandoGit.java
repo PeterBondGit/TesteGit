4615b4f9e0b44eeceebfdbdc7f58ea1e759d2811
Peter Bond <peterebond@gmail.com>
2022-04-29 14:55:43 -0300

    commitando mais uma vez

--- src/TestandoGit.java
+++ src/TestandoGit.java
@@ -1,6 +1,15 @@
 
 public class TestandoGit {
+	
+	private Long id;
+	private String nome;
+	
+	
+	
 	public static void main(String[] args) {
+		System.out.println("ana aqui");
+		System.out.println("ana aqui");
+		System.out.println("ana aqui");
 		System.out.println("ana aqui");
 	}
 }
